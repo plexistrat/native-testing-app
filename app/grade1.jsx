447ac128a7ef5ea18c91059e0c98d194bf0819ec
plexistrat <drosostaikos@yahoo.gr>
2025-08-01 19:16:24 +0300

    test

--- app/grade1.jsx
+++ app/grade1.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import GradeComponents from "./components/GradeComponents";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
 export default function grade1() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -12,6 +13,18 @@ export default function grade1() {
           <GradeComponents title="LeadGuitar" />
           <GradeComponents title="SpokenTests" />
           <GradeComponents title="AuralTests" />
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              For more details go to
+              <Link href="/listenExamples" style={{ color: "#DEE3E4" }}>
+                <Text> Listen examples</Text>
+              </Link>
+              .
+            </Text>
+            <Text style={styles.title}>
+              There you can find many audio examples
+            </Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -36,12 +49,14 @@ const styles = StyleSheet.create({
     borderColor: "#20c997",
     borderRadius: 20,
     borderWidth: 2,
+    marginTop: 20,
   },
   title: {
-    fontSize: 48,
+    fontSize: 18,
     fontWeight: "bold",
     color: "#DEE3E4",
-    marginBottom: 30,
+    marginBottom: 20,
+    marginTop: 20,
     textAlign: "center",
     color: "#20c997",
   },
